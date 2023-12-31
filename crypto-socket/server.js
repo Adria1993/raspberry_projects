c060b53734c06fdda3167f1b9482f709e63f4d70
Adrikus <adria.cabezuelo@gmail.com>
2023-12-31 20:15:11 +0100

    added stringify

--- crypto-socket/server.js
+++ crypto-socket/server.js
@@ -10,8 +10,8 @@ const server = net.createServer((socket) => {
     switch(message){
       case 'init':
         let crptos = await cpto_tck.obtenerPrecios();
-        let detailed = {case: message, crypto: JSON.stringify(crptos)};
-        socket.write(detailed);
+        let detailed = {case: message, crypto: crptos};
+        socket.write(JSON.stringify(detailed));
       break;
       default:
         socket.write("NO ES CORRECTO");
